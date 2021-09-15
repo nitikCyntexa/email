63c13294b8ca3f9e6e5b19bd30a6315244d3f710
Nitik <nitik@cyntexa.com>
2021-09-15 15:48:31 +0530

    rfsd

--- background.js
+++
@@ -1,8 +0,0 @@
-let active_tab_id =0;
-chrome.tabs.onActivated.addListener ( tab => {
-   
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        active_tab_id =  tab.tabId ;
-        chrome.tabs.executeScript(null, { file : './app.js'}, () => console.log("Hey app page "))
-    });
-});
